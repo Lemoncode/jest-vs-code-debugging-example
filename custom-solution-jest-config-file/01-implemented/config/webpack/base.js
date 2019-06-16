73a8f393b028a8b48a9f8fc0264f402434f38fc8
Braulio Diez Botella <braulio.diez@gmail.com>
2019-06-16 12:38:23 +0200

    config completed

---
+++ custom-solution-jest-config-file/01-implemented/config/webpack/base.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const merge = require('webpack-merge');
+const helpers = require('./helpers');
+
+module.exports = merge(
+  {},
+  {
+    context: helpers.resolveFromRootPath('src'),
+    resolve: {
+      alias: {
+        '@material-ui/core': '@material-ui/core/es',
+        '@material-ui/icons': '@material-ui/icons/es',
+        common: helpers.resolveFromRootPath('src/common'),
+        core: helpers.resolveFromRootPath('src/core'),
+        layout: helpers.resolveFromRootPath('src/layout'),
+        pods: helpers.resolveFromRootPath('src/pods'),
+        scenes: helpers.resolveFromRootPath('src/scenes'),
+      },
+      extensions: ['.js', '.ts', '.tsx'],
+    },
+    entry: {
+      app: ['regenerator-runtime/runtime', './index.tsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          loader: 'awesome-typescript-loader',
+          options: {
+            useBabel: true,
+            useCache: true,
+            babelCore: '@babel/core',
+          },
+        },
+      ],
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'all',
+            name: 'vendor',
+            test: /[\\/]node_modules[\\/]/,
+            enforce: true,
+          },
+        },
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'index.html',
+      }),
+      new CheckerPlugin(),
+    ],
+  }
+);
