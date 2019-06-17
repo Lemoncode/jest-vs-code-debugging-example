8b6476fd7baf3077a71fa87f7b419bbc2e827d81
Braulio <braulio.diez@gmail.com>
2019-06-17 18:57:46 +0200

    fixed unit test

--- create-react-app/00-start/src/calculator.test.js
+++ create-react-app/00-start/src/calculator.test.js
@@ -1,6 +1,7 @@
-import { sum } from './calculator';
+import { sum } from "./calculator";
+import { exportAllDeclaration } from "@babel/types";
 
-it('sums two numbers', () => {
+it("sums two numbers", () => {
   // Arrange
   const a = 2;
   const b = 3;
